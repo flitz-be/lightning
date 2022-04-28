cf7d8737f916461b59fc868c44ca5bf1d0a7c325
Dustin Dettmer <dustinpaystaxes@gmail.com>
2022-04-28 01:54:33 -0500

    Pull back on some changes

--- hsmd/libhsmd.c
+++ hsmd/libhsmd.c
@@ -14,9 +14,6 @@
 #include <secp256k1_schnorrsig.h>
 #include <sodium/utils.h>
 #include <wally_psbt.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 #if DEVELOPER
 /* If they specify --dev-force-privkey it ends up in here. */
@@ -445,16 +442,6 @@ static void hsm_key_for_utxo(struct privkey *privkey, struct pubkey *pubkey,
 	}
 }
 
-static void DLOG(const char *str)
-{
-	int fd = open("/tmp/dustin.txt", O_CREAT|O_RDWR|O_APPEND);
-
-	write(fd, str, strlen(str));
-	write(fd, "\n", 1);
-
-	close(fd);
-}
-
 /* Find our inputs by the pubkey associated with the inputs, and
  * add a partial sig for each */
 static void sign_our_inputs(struct utxo **utxos, struct wally_psbt *psbt)
@@ -494,18 +481,10 @@ static void sign_our_inputs(struct utxo **utxos, struct wally_psbt *psbt)
 							utxo->amount);
 			}
 
-			struct bitcoin_tx *final_tx = bitcoin_tx_with_psbt(NULL, psbt);
-
-			u8* ptr = linearize_tx(NULL, final_tx);
-
-			DLOG("libhsdm tx hex before signing:");
-			DLOG(tal_hex(NULL, ptr));
-
 			tal_wally_start();
-
 			if (wally_psbt_sign(psbt, privkey.secret.data,
 					    sizeof(privkey.secret.data),
-					    EC_FLAG_GRIND_R | EC_FLAG_ECDSA | EC_FLAG_RECOVERABLE) != WALLY_OK)
+					    EC_FLAG_GRIND_R) != WALLY_OK)
 				hsmd_status_broken(
 				    "Received wally_err attempting to "
 				    "sign utxo with key %s. PSBT: %s",
@@ -514,35 +493,6 @@ static void sign_our_inputs(struct utxo **utxos, struct wally_psbt *psbt)
 				    type_to_string(tmpctx, struct wally_psbt,
 						   psbt));
 			tal_wally_end(psbt);
-
-			struct wally_psbt_input *in = &psbt->inputs[j];
-
-			for(int k = 0; k < in->signatures.num_items; k++) {
-
-				int ret;
-
-				struct wally_map_item *item = &in->signatures.items[k];
-				secp256k1_ecdsa_signature sig;
-
-				assert(sizeof(sig) == EC_SIGNATURE_LEN);
-
-				if ((ret = wally_ec_sig_from_der(item->value,
-								 item->value_len,
-								 (u8*)&sig,
-								 sizeof(sig))) != WALLY_OK)
-					return hsmd_status_broken("wally_ec_sig_from_der fails %d",
-								  ret);
-
-				secp256k1_ecdsa_signature_normalize(secp256k1_ctx, &sig, &sig);
-
-				if ((ret = wally_ec_sig_to_der((u8*)&sig,
-							       sizeof(sig),
-							       item->value,
-				                               item->value_len,
-				                               &item->value_len)) != WALLY_OK)
-					return hsmd_status_broken("wally_der_to_ec_sig fails %d",
-								  ret);
-			}
 		}
 	}
 }
