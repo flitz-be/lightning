9f70b7ccf4cd40595b09f257e90483969e40a9e2
Dustin Dettmer <dustinpaystaxes@gmail.com>
2022-09-09 16:25:02 -0400

    WIP splicing code

--- openingd/dualopend.c
+++ openingd/dualopend.c
@@ -1986,6 +1986,7 @@ static u8 *accepter_commits(struct state *state,
 #else
 	commit_msg = towire_commitment_signed(NULL, &state->channel_id,
 					      &local_sig.s, NULL);
+#endif
 
 	peer_write(state->pps, take(commit_msg));
 
@@ -3958,8 +3959,6 @@ int main(int argc, char *argv[])
 		tal_steal(state->tx_state, state->tx_state->psbt);
 	} else
 		master_badmsg(fromwire_peektype(msg), msg);
-
-
 
 	/* 3 == peer, 4 = hsmd */
 	state->pps = new_per_peer_state(state);
